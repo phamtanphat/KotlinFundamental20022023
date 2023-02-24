4400755d1a1ff1827b66491b67533b573cc46a12
Phat <phatdroid94@gmail.com>
2023-02-24 21:14:11 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+//     Dấu // gọi comment
+//     1: Khai báo biến
+//     mutable, immutable
+//
+//     Từ khóa khai báo biến là var
+//    var name = "Phạm Tấn Phát"
+//    name = "Phạm Tấn Phát 1"
+//    print(name)
+//     Từ khóa khai báo val
+//    val pi = 3.14
+//    val address = "199 Lý thường kiệt"
+
+}
