0dc09144a29548f0ff65bc476ede2b11bbb80963
Phat <phatdroid94@gmail.com>
2023-02-24 21:37:29 +0700

    kieu du lieu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,4 +11,17 @@ fun main(args: Array<String>) {
 //    val pi = 3.14
 //    val address = "199 Lý thường kiệt"
 
+//     2: Kiểu dữ liệu
+//     Kiểu String: Chuỗi
+//    var text: String = "Hello"
+//     Kiểu số nguyên: Int, Long
+//    var age: Int = 29
+//     Kiểu số thực: Float, Double
+//    var weight: Float = 74.9f
+//    var query = """
+//        SELECT * FROM TableName
+//        WHERE ID = 1
+//        ORDER BY DESC
+//    """.trimIndent()
+//    print(query)
 }
